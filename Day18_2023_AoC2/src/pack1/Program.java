fa71ef612690b1aaadb81efdf3d8ee9482ce4669
dogonbb <darius.marzisch@gmx.de>
2024-05-25 19:48:35 +0200

    added Algorithm for digging but not for counting

--- Day18_2023_AoC2/src/pack1/Program.java
+++ Day18_2023_AoC2/src/pack1/Program.java
@@ -25,5 +25,76 @@ public class Program {
 		}
 		System.out.println(Arrays.toString(input));
 		////////////////////////////////////////////
+		int maxDown = 0;
+		int maxRight = 0; 
+		for(String s : input) {
+			if(s.charAt(0) == 'D') {
+				maxDown +=	Integer.parseInt(Character.toString(s.charAt(2))); 
+			}
+			if(s.charAt(0) == 'R') {
+				maxRight +=	Integer.parseInt(Character.toString(s.charAt(2))); 
+			}
+		}
+		int inkr = 200; 
+		int map[][] = new int[maxDown -400][maxRight + 1 + inkr]; //guessed inkr and -400
+		//1 - digged
+		Point currentPoint = new Point(inkr, inkr); 
+		map[currentPoint.y][currentPoint.x] = 1; 
+		
+	
+		for(String s : input) {
+			if(s.charAt(0) == 'D') {
+				int a = Integer.parseInt(Character.toString(s.charAt(2))); 
+				while(a > 0) {
+					currentPoint.y++;
+					map[currentPoint.y][currentPoint.x] = 1; 
+					a--; 
+				}
+			}
+			if(s.charAt(0) == 'R') {
+				int a = Integer.parseInt(Character.toString(s.charAt(2))); 
+				while(a > 0) {
+					currentPoint.x++;
+					map[currentPoint.y][currentPoint.x] = 1; 
+					a--; 
+				}
+			}
+			if(s.charAt(0) == 'U') {
+				int a = Integer.parseInt(Character.toString(s.charAt(2))); 
+				while(a > 0) {
+					currentPoint.y--;
+					map[currentPoint.y][currentPoint.x] = 1; 
+					a--; 
+				}
+			}
+			if(s.charAt(0) == 'L') {
+				int a = Integer.parseInt(Character.toString(s.charAt(2))); 
+				while(a > 0) {
+					currentPoint.x--;
+					map[currentPoint.y][currentPoint.x] = 1; 
+					a--; 
+				}
+			}
+		}
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map[0].length; j++) {
+				if(map[i][j] == 1) {
+					System.out.print("#");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
+class Point {
+	int x, y; 
+	
+	public Point(int x, int y ) {
+		this.x = x; 
+		this.y = y; 
 	}
 }
